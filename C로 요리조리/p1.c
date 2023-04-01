1cf495bc4026ebdcdb666375c8ce61cfdbbbb73d
yorirun <117011631+yorirun@users.noreply.github.com>
2023-04-01 20:11:09 +0900

    p1.c
    대입(자율동아리) 첫 시간 설명용으로 만든 예제 파일
    (깔끔하게 정리해서 여기 다시 올렸습니다.)

---
+++ C로 요리조리/p1.c
@@ -0,0 +1,8 @@
+#include <stdio.h> // 출력구문, 기본적인 역할 --> printf 명령어 위해 불러옴
+#include <iostream> //cmd불러오는 친구
+
+void main() { //main함수 실행 영역
+	printf_s("Hello World!"); //출력 , 끝내기 (;)
+}
+
+//참고 //는 주석
